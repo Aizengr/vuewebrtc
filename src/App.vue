1ea4a047369c7a538dea301894e277a1964ad76c
Aizengr <capacapass@gmail.com>
2023-04-30 02:00:42 +0300

    Small start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-homepage></the-homepage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHomepage from "./components/Layout/TheHomepage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHomepage,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
