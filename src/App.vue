2345adb14af6be28dce24792d610ca014f6558f2
Aizengr <capacapass@gmail.com>
2023-05-02 00:23:08 +0300

    Progress on NewRoom component and style changes

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,8 @@ body {
   margin: 0px !important;
   padding: 0px !important;
   background: url(./assets/background.jpg);
-  /* background: linear-gradient(180deg, #a13358, #4b2c38, #000000, #2e0412); */
-  background-size: 100% 100%;
+  /* background: linear-gradient(180deg, #0e0d0d, #312d2f, #615c5c, #2e0412); */
+  background-size: 120% 120%;
   animation: gradient 30s ease infinite;
   height: 100vh;
   font-weight: 400;
