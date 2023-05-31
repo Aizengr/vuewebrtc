bde97e7501396a03e0261f625118873fdbeee7bd
Aizengr <capacapass@gmail.com>
2023-06-01 00:04:00 +0300

    Updates on store as well as socket connectivity and error messages if server is down

---
+++ frontend/src/components/Layout/ServerDown.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="v-sep">
+    <p>Server unavailable! Please try again later.</p>
+  </div>
+  <base-button
+    @click="refreshPage"
+    class="centered"
+    buttonText="OK"
+  ></base-button>
+</template>
+
+<script>
+import BaseButton from "../UI/BaseButton.vue";
+export default {
+  components: {
+    BaseButton,
+  },
+  methods: {
+    refreshPage() {
+      location.reload();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-sep {
+  color: white;
+  font-size: x-large;
+  font-family: inherit;
+  -webkit-font-smoothing: antialiased;
+}
+
+p {
+  margin: auto;
+  padding: 2rem;
+}
+
+.centered {
+  margin: auto;
+  display: flex;
+}
+</style>
