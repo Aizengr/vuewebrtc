2a23275c150dde6566088c0d2220e060a62b896f
Aizengr <capacapass@gmail.com>
2023-09-19 22:44:47 +0300

    Background changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,15 +17,8 @@ body {
   margin: 0px !important;
   padding: 0px !important;
   height: 100vh;
-  background: linear-gradient(
-    180deg,
-    #181818,
-    #131313,
-    #000000,
-    rgb(22, 2, 9),
-    rgb(41, 4, 17)
-  );
-  animation: gradient 15s ease infinite;
+  background-color: #1e1e1f;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%23040404' fill-opacity='0.2' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   font-weight: 400;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
